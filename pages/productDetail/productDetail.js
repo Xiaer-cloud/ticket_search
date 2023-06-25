6d85d15b0dcdacc891b3ebf717c82b5d79a02c25
Xiaer-cloud <1010346531@qq.com>
2023-06-25 16:43:34 +0800

    机票列表渲染优化

--- pages/productDetail/productDetail.js
+++ pages/productDetail/productDetail.js
@@ -1,22 +1,31 @@
 Page({
   data:{
-    picSrc: '../../images/hotelPic.png'
-  },
-hotel:function(){
-          wx.redirectTo({
-          url: '../hotelDetails/hotelDetails',
-
-    })
-    },
-  //更换机票
-  changeFlight:function(){
-    wx.navigateTo({
-      url: '../airticket/airticket'
-    })
+    flight: [
+      {
+        time: "2023/04/05",
+        startCity: "上海",
+        startTime: "10:30",
+        startAirPlane: "上海虹桥机场T2",
+        endCity: "北京",
+        endTime: "08:30",
+        endAirPlane: "首都国际机场T2",
+        via: "经停济南",
+        flight: "东方航空MU5130",
+        airPlane: "波音737（中)"
+      },
+      {
+        time: "2016/04/05",
+        start: "上海",
+        startTime: "10:30",
+        startAirPlane: "上海虹桥机场T2",
+        end: "北京",
+        endTime: "08:30",
+        endAirPlane: "首都国际机场T2",
+        via: "经停济南",
+        flight: "东方航空MU5130",
+        airPlane: "波音737（中)"
+      }
+    ]
   }
-
-
-
-
 
 })
