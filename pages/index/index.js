6cfe2cc62fdc3b4b460abb980d0c6cdd489a0d78
Xiaer-cloud <1010346531@qq.com>
2023-09-06 15:10:46 +0800

    折磨

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,10 @@
 Page({
   data: {
+    listData: '',
     time: '2023-06-25',
-    currentIndex:1,
     startCity:'上海',
     endCity:'北京',
+    currentIndex:1,
     hotelCity:'上海',
     adultNum: 1,
     childNum: 1,
@@ -29,16 +30,102 @@ Page({
     })
   },
 
-  searchProduct: function(){
-    wx.navigateTo({
-      url:'../productDetail/productDetail'
+  isChina: function(s) {
+    var re=/[^\u4e00-\u9fa5]/;  
+    if(re.test(s)) return false;  
+    return true; 
+  },
+
+  test() {
+    wx.request({
+      // 注意，如果小程序开启校验合法域名时必须使用https协议
+      //在测试的情况下可以不开启域名校验
+      url: 'http://192.3.24.71:80/ticket/getData',
+      data: {
+      },
+      // 请求的方法
+      method: 'GET', // 或 ‘POST’
+      // 设置请求头，不能设置 Referer
+      header: {
+    		'content-type': 'application/json' // 默认值
+  	  },
+  	  // 请求成功时的处理
+      success: function (res) {
+        // 一般在这一打印下看看是否拿到数据
+        if (res.statusCode == 200) {
+          wx.setStorageSync("list", res.data.data)
+          var list = wx.getStorageSync("list");
+          console.log(list)
+        }
+      },
+      // 请求失败时的一些处理
+      fail: function () {
+      	wx.showToast({
+            icon: "none",
+            mask: true,
+          	title: "接口调用失败，请稍后再试。",
+         });
+      }
     })
+  },
+
+  searchProduct: function(){
+    this.test()
+    wx.setStorageSync('startCity', this.data.startCity)
+    wx.setStorageSync('endCity', this.data.endCity)
+    wx.setStorageSync('time', this.data.time)
+    console.log(this.isChina(this.data.startCity))
+    console.log(this.data.startCity)
+    if(this.isChina(this.data.startCity)==false){
+      wx.showModal({
+        title: '提示',
+        content: '请输入正确的出发地',
+        success: function (res) {
+          if (res.confirm) {//这里是点击了确定以后
+            console.log('用户点击确定')
+          } else {//这里是点击了取消以后
+            console.log('用户点击取消')
+          }
+        }
+      })
+    }
+    else if(this.isChina(this.data.endCity)==false){
+      wx.showModal({
+        title: '提示',
+        content: '请输入正确的到达地',
+        success: function (res) {
+          if (res.confirm) {//这里是点击了确定以后
+            console.log('用户点击确定')
+          } else {//这里是点击了取消以后
+            console.log('用户点击取消')
+          }
+        }
+      })
+    }
+    else{
+        wx.navigateTo({
+        url:'../productDetail/productDetail'
+      })
+    }
   },
 
   bindTimeChange: function(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       time: e.detail.value
+    })
+  },
+
+  changeStart(e) {
+    console.log(e.detail.value)
+    this.setData({
+        startCity : e.detail.value
+    })
+  },
+  changeEnd(e) {
+    console.log(e.detail.value)
+    this.setData({
+        endCity : e.detail.value
     })
   }
 })
--- pages/productDetail/productDetail.js
+++ pages/productDetail/productDetail.js
@@ -1,5 +1,9 @@
 Page({
   data:{
+    startCity: '',
+    endCity: '',
+    time: '',
+    listData: '',
     flight: [
       {
         time: "2023/04/05",
@@ -15,10 +19,54 @@ Page({
       },
       {
         time: "2016/04/05",
-        start: "上海",
+        startCity: "上海",
         startTime: "10:30",
         startAirPlane: "上海虹桥机场T2",
-        end: "北京",
+        endCity: "北京",
+        endTime: "08:30",
+        endAirPlane: "首都国际机场T2",
+        via: "经停济南",
+        flight: "东方航空MU5130",
+        airPlane: "波音737（中)"
+      },{
+        time: "2023/04/05",
+        startCity: "上海",
+        startTime: "10:30",
+        startAirPlane: "上海虹桥机场T2",
+        endCity: "北京",
+        endTime: "08:30",
+        endAirPlane: "首都国际机场T2",
+        via: "经停济南",
+        flight: "东方航空MU5130",
+        airPlane: "波音737（中)"
+      },{
+        time: "2023/04/05",
+        startCity: "上海",
+        startTime: "10:30",
+        startAirPlane: "上海虹桥机场T2",
+        endCity: "北京",
+        endTime: "08:30",
+        endAirPlane: "首都国际机场T2",
+        via: "经停济南",
+        flight: "东方航空MU5130",
+        airPlane: "波音737（中)"
+      },{
+        time: "2023/04/05",
+        startCity: "上海",
+        startTime: "10:30",
+        startAirPlane: "上海虹桥机场T2",
+        endCity: "北京",
+        endTime: "08:30",
+        endAirPlane: "首都国际机场T2",
+        via: "经停济南",
+        flight: "东方航空MU5130",
+        airPlane: "波音737（中)"
+      },{
+        time: "2023/04/05",
+        startCity: "上海",
+        startTime: "10:30",
+        startAirPlane: "上海虹桥机场T2",
+        endCity: "北京",
         endTime: "08:30",
         endAirPlane: "首都国际机场T2",
         via: "经停济南",
@@ -26,6 +74,20 @@ Page({
         airPlane: "波音737（中)"
       }
     ]
+  },
+
+  onLoad: function(){
+    var list = wx.getStorageSync("list")
+    var startCity = wx.getStorageSync('startCity')
+    var endCity = wx.getStorageSync('endCity')
+    var time = wx.getStorageSync('time')
+    console.log(list)
+    this.setData({
+      startCity: startCity,
+      endCity: endCity,
+      time: time,
+      listData : list
+    })
   }
 
 })
